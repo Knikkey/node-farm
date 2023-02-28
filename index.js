6825f174c4cce1fe01ce0a6ee3a01d4b93fbc27a
Knikkey <102648803+Knikkey@users.noreply.github.com>
2023-02-28 15:50:15 +0900

    initialized project

---
+++ index.js
@@ -0,0 +1,96 @@
+// const fs = require("fs");
+
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+
+// const textOut = `This is what we know about the avocado: ${textIn}. \nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File has been written");
+
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("file written");
+//       });
+//     });
+//   });
+// });
+
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+//templates
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+//functions
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) {
+    output = output.replace(
+      /{%NOT_ORGANIC%}/g,
+      product.organic ? "" : "not-organic"
+    );
+  }
+
+  return output;
+};
+
+//data
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  //overview
+  if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const cardsHTML = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHTML);
+    res.end(output);
+  }
+  //product
+  else if (pathName === "/product") {
+    res.end("This is the product");
+  }
+  //api
+  else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  }
+  //not found
+  else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listening to requests on port 8000");
+});
